72f40042013d52b0cec8201adcbe299a8c463b6e
triozer <boirardcedric@gmail.com>
2024-04-30 15:42:32 +0200

    feat: add support for non-ui plugins

--- src/components/framer-plugin.tsx
+++ src/components/framer-plugin.tsx
@@ -1,30 +1,60 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAutoSizer } from "../hooks/auto-sizer";
+import { UIOptions, framer } from "framer-plugin";
+import localforage from "localforage";
 
 type NumberValue = `0` | `${number}px`;
 
-type FramerPluginProps = {
-  children?: React.ReactNode;
-  padding?:
+type FramerPluginRealProps = {
+  padding:
     | `${NumberValue} ${NumberValue} ${NumberValue} ${NumberValue}`
     | `${NumberValue} ${NumberValue} ${NumberValue}`
     | `${NumberValue} ${NumberValue}`
     | `${NumberValue}`;
-  gap?: `${NumberValue} ${NumberValue}` | `${NumberValue}`;
-  autoResize?: boolean;
-} & React.HTMLAttributes<HTMLDivElement>;
+  gap: `${NumberValue} ${NumberValue}` | `${NumberValue}`;
+  autoResize: boolean;
+  uiOptions: UIOptions;
+  showOnMounted: boolean;
+};
+
+type FramerPluginProps = {
+  children?: React.ReactNode;
+} & Partial<FramerPluginRealProps> &
+  React.HTMLAttributes<HTMLDivElement>;
 
 const FramerPlugin = React.forwardRef<HTMLDivElement, FramerPluginProps>(
-  ({ children, padding, gap, autoResize, ...props }, ref) => {
-    const defaultProps = {
+  (
+    { children, padding, gap, autoResize, showOnMounted, uiOptions, ...props },
+    ref
+  ) => {
+    const defaultProps: FramerPluginRealProps = {
       padding: "0 15px 15px 15px",
       gap: "10px",
       autoResize: false,
+      showOnMounted: true,
+      uiOptions: {
+        title: "My Plugin",
+        position: "top right",
+        width: 240,
+        height: 95,
+        ...uiOptions,
+      },
     };
 
     const mergedProps = { ...defaultProps, ...props };
 
+    if (mergedProps.showOnMounted) {
+      framer.showUI(mergedProps.uiOptions);
+    }
+
     const { ref: mainRef } = useAutoSizer();
+
+    useEffect(() => {
+      localforage.config({
+        name: mergedProps.uiOptions?.title || "My Plugin",
+        storeName: "stores",
+      });
+    }, [showOnMounted, uiOptions]);
 
     return (
       <main
