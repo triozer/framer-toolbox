b8f990215ce7dc66813de08f6b398234c21a22d4
triozer <boirardcedric@gmail.com>
2024-05-01 16:07:05 +0200

    fix: css file path

--- examples/basic-plugin/src/main.tsx
+++ examples/basic-plugin/src/main.tsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import "@triozer/framer-toolbox/dist/styles.css";
+import "@triozer/framer-toolbox/dist/index.css";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
