c639f8d6dd80b23a7b9ad60120d312e0b10336d2
Cédric <boirardcedric@gmail.com>
2024-05-02 23:38:49 +0200

    Small cleaning up (#6)
    fix: bit of cleanup here and there

--- examples/basic-plugin/src/App.tsx
+++ examples/basic-plugin/src/App.tsx
@@ -1,57 +1,29 @@
-import { ReactNode, useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 
-import {
-  FramerPlugin,
-  useSelection,
-  useStore,
-  Button,
-} from "@triozer/framer-toolbox";
+import { FramerPlugin, useStore } from "@triozer/framer-toolbox";
 
-export function App() {
-  const selection = useSelection();
+const Home = () => {
   const [store, _, setStoreValue] = useStore("store", {
     lastVisit: -1,
   });
   const { lastVisit } = store;
 
-  const [els, setEls] = useState<ReactNode[]>([]);
-
   useEffect(() => {
     setStoreValue("lastVisit", Date.now());
   }, []);
 
-  const addElement = () => {
-    setEls((prev) => [
-      ...prev,
-      <p key={new Date().toLocaleTimeString() + Math.random()}>
-        {Math.random()}
-        {Math.random()}
-        {Math.random()}
-        {Math.random()}
-        {Math.random()}
-        {Math.random()}
-      </p>,
-    ]);
-  };
-
   return (
-    <FramerPlugin autoResize={true}>
-      <span>
-        Welcome{" "}
-        {lastVisit !== -1
-          ? `back (${new Date(lastVisit).toLocaleDateString()})`
-          : "to my plugin"}
-        !
-      </span>
-      <p>You have {selection.length} layers selected.</p>
-      <p>You have {selection.length} layers selected.</p>
-      <p>You have {selection.length} layers selected.</p>
-      <p>You have {selection.length} layers selected.</p>
-      <p>You have {selection.length} layers selected.</p>
-      <p>You have {selection.length} layers selected.</p>
-      <Button onClick={addElement}>Add Element</Button>
-      {els}
-    </FramerPlugin>
+    <span>
+      Welcome{" "}
+      {lastVisit !== -1
+        ? `back (${new Date(lastVisit).toLocaleDateString()})`
+        : "to my plugin"}
+      !
+    </span>
   );
+};
+
+export function App() {
+  return <FramerPlugin autoResize={true} />;
 }
